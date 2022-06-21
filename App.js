6ac16f91349d7e59c33ab16d750d69f8f2641d3e
codewithdarkwa <jnrdrkw1@gmail.com>
2022-06-21 13:23:51 +0000

    successfully fetch the meals with it's content

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Appbar, Searchbar } from 'react-native-paper';
+import { Appbar, Searchbar,Card,Paragraph} from 'react-native-paper';
+import { ScrollView } from 'react-native-web';
 
 export default function App() {
- const [meals, setMeals] = useState([]);
+  const [meals, setMeals] = useState([]);
+  console.log(meals);
  const  [searchQuery, setSearchQuery] = useState('');
  const url ="https://www.themealdb.com/api/json/v1/1/categories.php";
 
  const getMeals = async () =>{
   const response = await fetch(url);
   const data = await response.json();
-   setMeals(data);
-   console.log(data);
+   setMeals(data.categories);
+   
  }
   useEffect(()=>{
     getMeals();
@@ -29,6 +31,19 @@ export default function App() {
       value={searchQuery}
       onChangeText={onChangeSearch}
       />
+      <ScrollView>
+        {
+          meals.map(meal=>(
+            <Card key={meal.idCategory}>
+              <Card.Cover source={meal.strCategoryThumb}/>
+              <Card.Title title={meal.strCategory}/>
+              <Card.Content>
+                <Paragraph>{meal.strCategoryDescription}</Paragraph>
+              </Card.Content>
+            </Card>
+          ))
+        }
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
