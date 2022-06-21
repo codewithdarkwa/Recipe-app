b175c803ab1763ce2f950da809a721d6821da80d
codewithdarkwa <jnrdrkw1@gmail.com>
2022-06-21 12:10:55 +0000

    fetch data from an api

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+ const [meals, setMeals] = useState([]);
+ const url ="https://www.themealdb.com/api/json/v1/1/categories.php";
+
+ const getMeals = async () =>{
+  const response = await fetch(url);
+  const data = await response.json();
+   setMeals(data);
+   console.log(data);
+ }
+  useEffect(()=>{
+    getMeals();
+  },[])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
