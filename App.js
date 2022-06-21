3e38d7e36726b948d53e94e79c721c24b85716d5
codewithdarkwa <jnrdrkw1@gmail.com>
2022-06-21 12:45:23 +0000

    added an appbar and a searchbar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Appbar, Searchbar } from 'react-native-paper';
 
 export default function App() {
  const [meals, setMeals] = useState([]);
+ const  [searchQuery, setSearchQuery] = useState('');
  const url ="https://www.themealdb.com/api/json/v1/1/categories.php";
 
  const getMeals = async () =>{
@@ -16,19 +18,27 @@ export default function App() {
     getMeals();
   },[])
 
+  const onChangeSearch = query =>setSearchQuery(query);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Appbar>
+        <Appbar.Content title="Recipe App"/>
+      </Appbar>
+      <Searchbar 
+      placeholder='search'
+      value={searchQuery}
+      onChangeText={onChangeSearch}
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
 });
